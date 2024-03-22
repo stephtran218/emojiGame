b70c51d40e16aa233fc46b52e2c5852382d7dd39
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-03-22 10:15:54 -0700

    3/22

--- emojiGame/emojiGame/GameView.swift
+++ emojiGame/emojiGame/GameView.swift
@@ -13,13 +13,18 @@ struct GameView: View {
     @State var emojis = ["😀", "😀" ,"😁", "😁", "😂", "😂", "🤣", "🤣", "😃", "😃", "😄", "😄",]
     let lightPink = Color(red: 255/255, green: 192/255, blue: 203/255)
     let lightGreen = Color(red: 172/255, green: 225/255, blue: 175/255 )
-    var score: Int = 0
+    @State private var score: Int = 0
     let columns: [GridItem] = [
         GridItem(.fixed(50), spacing: 60, alignment: nil),
         GridItem(.fixed(50), spacing: 60, alignment: nil),
         GridItem(.fixed(50), spacing: 60, alignment: nil),
-
     ]
+    @State private var numOfClicks: Int = 0
+    @State private var correctAnswers: Int = 0
+    
+    @State private var firstCardIndex: Int = -1
+    @State private var secondCardIndex: Int = -1
+    let correctNumPair: Int = 6
     
     var body: some View {
         ZStack{
@@ -30,9 +35,10 @@ struct GameView: View {
                 .foregroundColor(lightGreen)
                 .offset(CGSize(width: 90.0, height: 320.0))
             VStack{
-                Text("Current Score: \(score)")
+                Text("Current Score: \(score)%")
                     .font(.system(size: 25))
                     .padding(.bottom, 470)
+                    .bold()
             }
             LazyVGrid(columns: columns){
                 ForEach(emojis.indices, id: \.self){ i in
@@ -40,15 +46,57 @@ struct GameView: View {
                         SquareView(emoji: emojis[i], isFlipped: cardsFlipped[i])
                     })
                 }
+            }
+        }
+        .onAppear{
+            //This will shuffle the emojis' order in array for every new round
+            emojis.shuffle()
+        }
+    }
+    
+    //this function will handle the clicks
+    func handleClick(index: Int){
+        //when the bool is true, this means the user clicked on a square so the square goes from blue to clear, the index keeps track which "card" got flipped to check for accuracy
+        cardsFlipped[index] = true
+        numOfClicks += 1
                 
+        if firstCardIndex == -1 || secondCardIndex == -1{
+            if firstCardIndex == -1{
+                firstCardIndex = index
+                cardsFlipped[index] = true
+            } else {
+                secondCardIndex = index
+                cardsFlipped[index] = true
+                if emojis[firstCardIndex] == emojis[secondCardIndex]{
+                    correctAnswers += 1
+                    firstCardIndex = -1
+                    secondCardIndex = -1
+                    
+                    if correctAnswers == correctNumPair{
+                        calcScore()
+                    }
+                } else{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        cardsFlipped[firstCardIndex] = false
+                        cardsFlipped[secondCardIndex] = false
+                        
+                        self.firstCardIndex = -1
+                        self.secondCardIndex = -1
+                    }
+                }
             }
             
         }
-        
     }
-    func handleClick(index: Int){
-        cardsFlipped[index] = true
+    
+    func calcScore() {
+        if numOfClicks > 0 {
+            score = (correctAnswers * 100) / numOfClicks
+        } else {
+            score = 0
+        }
     }
+
 }
 
 
