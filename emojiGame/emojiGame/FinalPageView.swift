aeddbfb80ad277d418a0fbaebf07d6b3fc6950ad
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-03-25 10:22:31 -0700

    3/25

---
+++ emojiGame/emojiGame/FinalPageView.swift
@@ -0,0 +1,32 @@
+//
+//  FinalPageView.swift
+//  emojiGame
+//
+//  Created by StudentAM on 3/25/24.
+//
+
+import SwiftUI
+
+struct FinalPageView: View {
+    var body: some View {
+        VStack {
+                Text("😃")
+                    .font(.system(size: 150))
+                Text("Great job!")
+                    .font(.title)
+//                Button(action: /*retry*/) {
+//                    Text("Try Again")
+//                        .font(.system(size: 25))
+//                        .frame(width: 150, height: 50)
+//                        .background(Color.blue)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
+//                        .padding()
+//                }
+            }
+    }
+}
+
+#Preview {
+    FinalPageView()
+}
--- emojiGame/emojiGame/GameView.swift
+++ emojiGame/emojiGame/GameView.swift
@@ -27,30 +27,54 @@ struct GameView: View {
     let correctNumPair: Int = 6
     
     var body: some View {
-        ZStack{
-            Circle()
-                .foregroundColor(lightPink)
-                .offset(CGSize(width: -90.0, height: -290))
-            Circle()
-                .foregroundColor(lightGreen)
-                .offset(CGSize(width: 90.0, height: 320.0))
-            VStack{
-                Text("Current Score: \(score)%")
-                    .font(.system(size: 25))
-                    .padding(.bottom, 470)
-                    .bold()
-            }
-            LazyVGrid(columns: columns){
-                ForEach(emojis.indices, id: \.self){ i in
-                    Button(action: {handleClick(index: i)}, label: {
-                        SquareView(emoji: emojis[i], isFlipped: cardsFlipped[i])
-                    })
+        NavigationView {
+            ZStack{
+                Circle()
+                    .foregroundColor(lightPink)
+                    .offset(CGSize(width: -90.0, height: -290))
+                Circle()
+                    .foregroundColor(lightGreen)
+                    .offset(CGSize(width: 90.0, height: 320.0))
+                VStack{
+                    Text("Current Score: \(score)%")
+                        .font(.system(size: 25))
+                        .padding(.bottom, 470)
+                        .bold()
                 }
+                LazyVGrid(columns: columns){
+                    ForEach(emojis.indices, id: \.self){ i in
+                        Button(action: {handleClick(index: i)}, label: {
+                            SquareView(emoji: emojis[i], isFlipped: cardsFlipped[i])
+                        })
+                    }
+                }
+                HStack{
+                    NavigationLink(destination: FinalPageView().navigationBarBackButtonHidden(true), label: {
+                        Text("Next")
+                            .font(.system(size: 25))
+                            .frame(width: 100, height: 50)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .bold()
+                        })
+                    Button(action: retry) {
+                        Text("Retry")
+                            .font(.system(size: 25))
+                            .frame(width: 100, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .bold()
+                    }
+
+                }
+                .padding(.top, 480)
             }
-        }
-        .onAppear{
-            //This will shuffle the emojis' order in array for every new round
-            emojis.shuffle()
+            .onAppear{
+                //This will shuffle the emojis' order in array for every new round
+                emojis.shuffle()
+            }
         }
     }
     
@@ -59,7 +83,7 @@ struct GameView: View {
         //when the bool is true, this means the user clicked on a square so the square goes from blue to clear, the index keeps track which "card" got flipped to check for accuracy
         cardsFlipped[index] = true
         numOfClicks += 1
-                
+        
         if firstCardIndex == -1 || secondCardIndex == -1{
             if firstCardIndex == -1{
                 firstCardIndex = index
@@ -97,10 +121,15 @@ struct GameView: View {
         }
     }
 
+    
+    func retry() {
+        score = 0
+        emojis.shuffle() // Shuffle emojis for a new game
+    }
 }
 
-
-
-#Preview {
-    GameView()
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
 }
